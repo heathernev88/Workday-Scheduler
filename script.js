06fa578b6a84434f4fd8030b90ff5cc7ac8dd6ab
HeatherNev88 <heather.alv88@yahoo.com>
2020-08-25 20:30:53 -0700

    finalized project, saves input to local storage, time blocks reflect past, present, or future

--- script.js
+++ script.js
@@ -1,66 +1,97 @@
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-let textArea = document.querySelector(".description");
-let timeNow = moment().format('HH');
-let timeSchedule = $(".hour").attr("data-time");
-let saveBtn = document.querySelector(".saveBtn");
-
-let input = localStorage.getItem("input");
-textArea.textContent = input;
+let textArea = $(".description");
+let timeNow = moment().hour();
+let timeSchedule = $(".time-block").attr("data-hour");
+let saveBtn = $(".saveBtn");
+let storedInfo = [];
 
 // show current date at top of document (day of week, month/date, year)
-    const container = document.querySelector(".container")
-    const currentDay= document.getElementById("currentDay")
+    const container = $(".container");
+    const currentDay= $("#currentDay");
     let today = moment().format('dddd, MMMM Do YYYY');
-    document.getElementById("currentDay").innerHTML = today;
+    $("#currentDay").text(today);
 // end current date
 
+// saves user input to local storage
+
+function saveLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
     
-
-
-// save user input to local storage 
-
-function saveInput() {
-    let storedData = localStorage.getItem("input")
-    textArea.textContent = storedData;
-
 }
 
-// end local storage
+function retrieveLocalStorage(key) {
+    storedInfo = JSON.parse(localStorage.getItem("info")) || [];
+    
+}
 
-// add even listener that when user clicks save image button input is saved into local storage
-
-saveBtn.addEventListener("click", function(event) {
+$(".saveBtn").on("click", function(event) {
     event.preventDefault();
-    userInput = document.querySelector("description").value;
-    localStorage.setItem("event", JSON.stringify(userInput));
+
+    let userInput = $(this).siblings(".description").val().trim();
+    let hour = $(this).parent().attr("id"); 
+    
+    let note = {
+        time: hour,
+        input: userInput
+    };
 
     
+    storedInfo.push(note);
+
+    saveLocalStorage("info", storedInfo);
     
-    saveInput();
-})
-    
 
+});
 
+function populateTimeSlots() {
+    let note;
+    if (!storedInfo) {
+        return;
+    }
 
-// add class of past, present, or future depending on what time it is
+    for (let i=0; i < storedInfo.length; i++) {
+
+        note = storedInfo[i];
+        $("#" + note.time).children(".description").val(note.input);
+
+    }
+
+};
+
+// end save user input to local storage
+
+// adds class of past, present, or future depending on what time it is
 
 function setColor() {
-    if (timeSchedule < timeNow ) {
-        textArea.addClass("past")
 
-    } else if (timeSchedule === timeNow) {
-    textArea.addClass("present")
+    let timeBlocks = $(".time-block");
+    let dataHour;
+    let timeBlock;
+    let date = moment().format("YYYY-MM-DD");
+    let currentTime = moment().format("HH");
+    currentTime = moment(currentTime, "HH");
+    let textArea;
 
-    } else {
-    textArea.addClass("future")
-   }
-}
+
+    for (var i = 0; i < timeBlocks.length; i++) {
+        timeBlock = $(timeBlocks[i]);
+        textArea = timeBlock.children(".description");
+        dataHour = timeBlock.data("hour");
+        dataHour = moment(dataHour, "HH");
+        
+        
+        if (currentTime.isBefore(dataHour)) {
+            textArea.addClass("future")
+        } else if (currentTime.isAfter(dataHour)) {
+            textArea.addClass("past");
+        } else {
+            textArea.addClass("present");
+        }
+    }
+};
+// end class of past, present, or future
+
+$(document).ready(function(){
+    retrieveLocalStorage("info");
+    populateTimeSlots();
+    setColor();
+}) ;
